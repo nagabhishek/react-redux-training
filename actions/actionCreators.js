4c34601452a342535f4a2638ea51de86d2ad9708
nagabhishek <nag.abhishek2012@gmail.com>
2022-05-18 10:14:11 +0530

    1 file added

---
+++ actions/actionCreators.js
@@ -0,0 +1,20 @@
+const INCREMENT_LIKES = 'INCREMENT_LIKES'; // best practice !
+const DELETE_COURSE = 'DELETE_COURSE';
+const ADD_NEW_COURSE = 'ADD_NEW_COURSE';
+const DELETE_POST = 'DELETE_POST';
+
+export function IncrementLikes() {
+  return { type: INCREMENT_LIKES };
+}
+
+export function DeleteCourse() {
+  return { type: DELETE_COURSE };
+}
+
+export function AddCourse() {
+  return { type: ADD_NEW_COURSE };
+}
+
+export function DeletePost() {
+  return { type: DELETE_POST };
+}
